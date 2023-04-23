5c8541205a9f955ed270e5f64c3a3e97af7c46f6
Eryck Barreto <eryck.barreto@hotmail.com>
2023-04-23 16:50:01 -0300

    Uso inicial de strings

---
+++ aula3_string.py
@@ -0,0 +1,26 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica (já sabe qual tipo de informação) / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+
+"""
+# aspas simples
+
+print('Eryck Barreto')
+
+# aspas duplas
+print("Eryck Barreto")
+
+# Escape - para o interpretador pular o caractere seguinte na interpretação
+print("Eryck \"Barreto\"")
+
+# r - caso queira que apareça a barra invertida por exemplo
+print(r"Eryck \"Barreto\"")
+
+# para o código ficar limpo, melhor usar aspas duplas dentro de aspas simples
+# é possível o contrário também, aspas simples dentro de duplas
+print(1, 'Eryck "Barreto"')
+print(2, "Eryck 'Barreto'")
+
