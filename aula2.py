66bead33d848314dd024ec68b5af25b0cd93e7bb
Eryck Barreto <eryck.barreto@hotmail.com>
2023-04-23 16:49:02 -0300

    Como usar separadores

---
+++ aula2.py
@@ -0,0 +1,8 @@
+# no comando sep=" " (separados) é possível mudar
+# por padrão o que o vscode separa
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34, sep="-", end='\r\n')
+print(56, 78, sep=';', end='\n')
+# python diferencia letras maiúsculas e letras minúsculas
+
