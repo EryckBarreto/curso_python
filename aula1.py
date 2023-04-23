75aed77ac73e3c25af18fcec88a0e7d01acfcec6
Eryck Barreto <eryck.barreto@hotmail.com>
2023-04-23 16:48:49 -0300

    Aula sobre docstring

---
+++ aula1.py
@@ -0,0 +1,11 @@
+"""
+DocString, pode ser usado para digitar anotações com mais de uma linha
+podendo pular linha sem colocar o hashtag, no entanto
+o interpretador lê, então se possível utilizar o # mesmo
+Além das aspas duplas " pode ser usadas três aspas simples '''
+"""
+
+
+# comentários acima
+print(123) # ao lado
+# abaixo também
